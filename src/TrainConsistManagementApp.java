36657e0ea9230005d770df0ac22a22a522b7b436
NIKHILBHUSHAN <nikhilbhushan95824@gmail.com>
2026-03-27 15:06:01 +0530

    UC2: Add Passenger Bogies to Train (ArrayList Operations)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
     public static void main(String[] args){
         System.out.println("=====================================");
-        System.out.println("  == Train consist Management App ==  ");
+        System.out.println("   UC2-Add Passenger Bogies to train ");
         System.out.println("=====================================");
-        System.out.println("Train Initialized Succesfuly");
-        System.out.println("Intial Boggie count: 0");
-        System.out.println("Current train consist: []");
-        System.out.println("System ready for operations");
+        List<String> passengerBogies=new ArrayList<>();
+        passengerBogies.add("sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("FirstClass");
+        System.out.println("After adding boggies:\nPassenger Boggies:"+passengerBogies);
+        passengerBogies.remove("AC Chair");
+        System.out.println("After removing AC Chair:\nPassenger Boggies:"+passengerBogies);
+        System.out.println("Checking if Sleeper exists:\nContains Sleeper? :"+passengerBogies.contains("sleeper"));
+        System.out.println("Final Passenger Consist:\n"+passengerBogies);
+        System.out.println("UC2 operations completed succesfully");
+
     }
 
 }
